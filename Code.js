aafef082200706ff01cad7251b0b027b4c551767
Szabó Bence <szabo.bence.tat@gmail.com>
2020-02-24 07:58:03 +0100

    refactor to single function for urls reading

--- Code.js
+++ Code.js
@@ -1,12 +1,11 @@
 function main() {
-  var urlsToCheck = readUrls();
+  var urlsToCheck = getUrlsFromSheetAndColumn('landing pages with status code', 4);
 
   return 0;
 }
 
-function readUrls() {
-  var columnIndex = 4;
-  var sheet = SpreadsheetApp.getActive().getSheetByName('landing pages with status code');
+function getUrlsFromSheetAndColumn(sheetName, columnIndex) {
+  var sheet = SpreadsheetApp.getActive().getSheetByName(sheetName);
   var values = sheet.getDataRange().getValues();
   var urls = [];
 
@@ -17,20 +16,4 @@ function readUrls() {
   return urls.filter(function (url) {
     return url != '';
   });
-}
-
-/**
- * Validates the provided spreadsheet URL to make sure that it's set up
- * properly. Throws a descriptive error message if validation fails.
- *
- * @param {string} spreadsheetId The URL of the spreadsheet to open.
- * @return {Spreadsheet} The spreadsheet object itself, fetched from the URL.
- * @throws {Error} If the spreadsheet URL hasn't been set
- */
-function validateAndGetSpreadsheet(spreadsheetId) {
-  if (spreadsheetId === 'YOUR_SPREADSHEET_ID') {
-    throw new Error('Please specify a valid Spreadsheet URL. You can find' +
-      ' a link to a template in the associated guide for this script.');
-  }
-  return SpreadsheetApp.openById(spreadsheetId);
 }
